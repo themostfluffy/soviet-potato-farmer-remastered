943c9f4dbde55d02e6e4548bfc6e74d49e975420
themostfluffy <Fuzzyfox9876@gmail.com>
2026-09-15 16:00:00 +0000

    dgbsb

---
+++ draw.js
@@ -0,0 +1,6 @@
+function setup() {
+  createCanvas(1000, 600);
+}
+function draw() {
+  background(220);
+}
---
+++ econ.js
@@ -0,0 +1,7 @@
+//money 
+var cash=10
+//potatoes cost and invetory
+var potatoes=0
+var seeds=0
+var seedsCost=10
+var growthCooldown=5000
